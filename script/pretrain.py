07ea81e94c9dd2bd4c2644d08ce0406d0cee3153
Titus <wjt90325042@163.com>
2022-10-22 10:03:52 +0800

    修改了一下细节，这是最基础的版本

--- script/pretrain.py
+++ script/pretrain.py
@@ -20,7 +20,7 @@ def pretrain(model, Pretrain_p, dataset, data_loader, criterion, view, optimizer
             for v in range(view):
                 rec_list.append(criterion.mse(xs[v], rs[v]))
                 for w in range(v+1, view):
-                    iic_list.append(0.001*criterion.forward_iic(zs[v], zs[w]))
+                    iic_list.append(criterion.forward_iic(zs[v], zs[w]))
             rec = sum(rec_list)
             iic = sum(iic_list)
             tot = rec + iic
@@ -30,22 +30,22 @@ def pretrain(model, Pretrain_p, dataset, data_loader, criterion, view, optimizer
             loss_rec += rec.item()
             loss_iic += iic.item()
             loss_tot += tot.item()
-        if (Pretrain_p['p_epoch'] + 1) % Pretrain_p['p_interval'] == 0:
-            output = "Epoch : {:.0f}/{:.0f} ===> Reconstruction loss = {:.4f}" \
-                     "===> IIC loss = {:.4e} ===> Total Loss = {:.4e}" \
-                .format((epoch + 1), Pretrain_p['p_interval'], loss_rec, loss_iic, loss_tot)
-            print(output)
-        scores_each, scores_tot = evaluation(Pretrain_p, model, dataset, view, data_size, class_num, device)
-        tags = ["train_loss", "accuracy", "learning_rate"]
-        tb_writer.add_scalar(tags[0], loss_tot, epoch)
-        tb_writer.add_scalar(tags[1], scores_tot['kmeans']['accuracy'], epoch)
-        tb_writer.add_scalar(tags[2], optimizer.param_groups[0]["lr"], epoch)
-        recode(res_excel, epoch + 1, Pretrain_p['p_epoch'], loss_tot, scores_tot, scores_each)
+        output = "Epoch : {:.0f}/{:.0f} ===> Reconstruction loss = {:.4f}" \
+                 "===> IIC loss = {:.4e} ===> Total Loss = {:.4e}" \
+            .format((epoch + 1), Pretrain_p['p_epoch'], loss_rec, loss_iic, loss_tot)
+        print(output)
+        if (epoch+1) % Pretrain_p['p_interval'] == 0:
+            scores_each, scores_tot = evaluation(Pretrain_p, model, dataset, view, data_size, class_num, device)
+            tags = ["train_loss", "accuracy", "learning_rate"]
+            tb_writer.add_scalar(tags[0], loss_tot, epoch)
+            tb_writer.add_scalar(tags[1], scores_tot['kmeans']['accuracy'], epoch)
+            tb_writer.add_scalar(tags[2], optimizer.param_groups[0]["lr"], epoch)
+            recode(res_excel, epoch + 1, Pretrain_p['p_epoch'], loss_tot, scores_tot, scores_each)
         # save result
         # if epoch == x -1:
         #  torch.save(model.state_dict(), Pretrain_p['pretrain_path'])
 
-    pd.DataFrame(res_excel).to_excel(Pretrain_p['pretrain_save'])
+    #pd.DataFrame(res_excel).to_excel(Pretrain_p['pretrain_save'])
 
 
 
